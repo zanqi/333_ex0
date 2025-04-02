f8d5568fbd25915633a55536c878f1d86d6cafd5
Zanqi Liang <zanqil@cs.washington.edu>
2025-04-01 17:31:35 -0700

    style fix

--- ex0.c
+++ ex0.c
@@ -2,27 +2,25 @@
 // Name: Zanqi Liang
 // Email: zanqil@uw.edu
 
-#include <stdio.h>  // printf
-#include <stdlib.h> // EXIT_SUCCESS, EXIT_FAILURE
+#include <stdio.h>   // printf
+#include <stdlib.h>  // EXIT_SUCCESS, EXIT_FAILURE
+#include <stdint.h>
 
 // Estimate pi with n terms
-double pi(long n);
+double pi(int64_t n);
 
-int main(int argc, char const *argv[])
-{
-  if (argc != 2)
-  {
+int main(int argc, char const *argv[]) {
+  if (argc != 2) {
     printf("Error: Only accept 1 argument");
     return EXIT_FAILURE;
   }
 
   char *endptr;
   // If argv[1] is an integer, endptr will point to the end of string
-  long n = strtol(argv[1], &endptr, 10);
-  if (*endptr != '\0')
-  {
+  int64_t n = strtol(argv[1], &endptr, 10);
+  if (*endptr != '\0') {
     fprintf(stderr,
-            "Error: '%s' is not a valid long integer.\n", argv[1]);
+            "Error: '%s' is not a valid integer.\n", argv[1]);
     return EXIT_FAILURE;
   }
 
@@ -30,12 +28,10 @@ int main(int argc, char const *argv[])
   return EXIT_SUCCESS;
 }
 
-double pi(long n)
-{
+double pi(int64_t n) {
   int i;
   double res = 3;
-  for (i = 1; i <= n; i++)
-  {
+  for (i = 1; i <= n; i++) {
     double ii = i;
     res += ((i % 2 == 0) ? -1 : 1) *
            (4 / (2 * ii * (2 * ii + 1) * (2 * ii + 2)));
